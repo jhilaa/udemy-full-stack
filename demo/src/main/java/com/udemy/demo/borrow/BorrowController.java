121db0fa0a5d19fb2bacd55e7a127ec3aefc4f55
Julien Hilaire <julien.hilaire@imelmi.net>
2022-07-09 19:28:27 +0200

    19 controller Borrow

---
+++ demo/src/main/java/com/udemy/demo/borrow/BorrowController.java
@@ -0,0 +1,34 @@
+package com.udemy.demo.borrow;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+public class BorrowController {
+
+    @GetMapping(value="/borrows")
+    public ResponseEntity getMyBorrows() {
+        //TODO
+        Borrow borrow = new Borrow();
+        borrow.setAskDate(LocalDate.now());
+        return new ResponseEntity(borrow, HttpStatus.OK);
+    }
+
+    @PostMapping(value="/borrows/{bookId}")
+    public ResponseEntity createBorrow(@PathVariable("bookId") String bookId){
+        //TODO
+        Borrow borrow = new Borrow();
+        return new ResponseEntity(borrow, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value="/borrows/{borrowId}")
+    public ResponseEntity deleteBorrow(@PathVariable("borrowId") String borrowId){
+        //TODO
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+}
